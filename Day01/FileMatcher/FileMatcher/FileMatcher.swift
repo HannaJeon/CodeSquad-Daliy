d7a5ba1a30a98aff4be1605fad7464f6cc8b9a2e
Mac <mac@Macui-MacBook-Air.local>
2017-03-16 00:18:07 +0900

    BlueLevel Solution

--- Day01/FileMatcher/FileMatcher/FileMatcher.swift
+++ Day01/FileMatcher/FileMatcher/FileMatcher.swift
@@ -10,12 +10,56 @@ import Foundation
 
 class FileMatcher {
     
+    @discardableResult
     func allFiles(atPath : String) throws -> [String] {
-    let files = try FileManager.default.contentsOfDirectory(atPath: atPath)
-    return files
+        var files = try FileManager.default.contentsOfDirectory(atPath: atPath)
+        
+        //파일명 순서대로 정렬을 하려면?
+        files = files.sorted { (s1, s2) -> Bool in
+            return s1 < s2
+        }
+        return files
     }
-//    isExist(filename:String,
-//    atPath:String) -> Bool
+    
+    @discardableResult
+    func isExist(filename:String, atPath:String) throws -> Bool{
+        let file = try allFiles(atPath: atPath)
+        var result = false
+        
+        for i in file {
+            if i.range(of: filename) != nil {
+                result = true
+            }
+        }
+        return result
+    }
+// 플러스 미션
+//만약 찾아야 하는 파일이 여러 개라면?
+    @discardableResult
+    func someFiles(targetFile : [String], atPath : String) throws -> [String]{
+        let file = try allFiles(atPath: atPath)
+        var result = [String]()
+        
+        for i in targetFile {
+            result += file.filter({ $0 == i})
+        }
+        return result
+    }
+    
+//특정 확장자(.doc) 파일 목록만 구하려면?
+    @discardableResult
+    func docFiles(target : String, atPath : String) throws -> [String]{
+        let file = try allFiles(atPath: path)
+        var result = [String]()
+        
+        for val in file {
+            if val.range(of: target) != nil {
+                result.append(val)
+            }
+        }
+        return result
+    }
+    
     
 
 }
--- Day01/FileMatcher/FileMatcher/main.swift
+++ Day01/FileMatcher/FileMatcher/main.swift
@@ -8,5 +8,14 @@
 
 import Foundation
 
+let path = "/Users/mac/Desktop"
+
 var files = FileMatcher()
-try files.allFiles(atPath: "/Users/mac/Desktop/temp")
+try files.allFiles(atPath: path)
+try files.isExist(filename: "tempfile1.rtf", atPath: path)
+
+//만약 찾아야 하는 파일이 여러 개라면?
+try files.someFiles(targetFile: ["profile.jpeg", "Make IndexOf.js"], atPath: path)
+
+//특정 확장자(.doc) 파일 목록만 구하려면?
+try files.docFiles(target: ".doc", atPath: path)
